5a7c9c4d7f1f2e6c194e500d32f050e3f85e7b39
luskha <lucas.limaacesso@gmail.com>
2024-07-08 16:41:23 -0300

    alterando cards passeios

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -5,20 +5,21 @@ export const Services = (props) => {
     <div id="services" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Passeios</h2>
+          <h2>Passeios Turísticos</h2>
           <p>
-          Descubra os melhores destinos da região com nossos emocionantes passeios turísticos. Oferecemos diversas opções para você explorar paisagens deslumbrantes, praias paradisíacas e atrações únicas. Escolha seu roteiro e prepare-se para uma experiência inesquecível.
+            Descubra os melhores destinos da região com nossos emocionantes passeios turísticos. Oferecemos diversas opções para você explorar paisagens deslumbrantes, praias paradisíacas e atrações únicas. Escolha seu roteiro e prepare-se para uma experiência inesquecível.
           </p>
         </div>
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <div className="service-desc">
-                    <h3>{d.name}</h3>
-                    <p>{d.text}</p>
+                <div key={`${d.name}-${i}`} className="col-md-3">
+                  <div className="service-card">
+                    <i className={d.icon}></i>
+                    <div className="service-desc">
+                      <h3>{d.name}</h3>
+                      <p>{d.text}</p>
+                    </div>
                   </div>
                 </div>
               ))
